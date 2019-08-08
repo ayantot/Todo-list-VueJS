e0babc57cf992299a018016ccae7f0d8f16f9495
TEGNY Thomas <thomas.tegny06@gmail.com>
2019-08-08 08:55:22 +0200

    first commit to add my project Todo list with VueJS

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+
+<div>
+  <carousel>
+    <carousel-slide>
+      Salut les gens
+    </carousel-slide>
+    <carousel-slide>
+      Au revoir les gens
+    </carousel-slide>
+  </carousel>
+  <Todos></Todos>
+</div>
+
+</template>
+
+<script>
+import Todos from './components/Todos'
+import Carousel from './components/carousel/Carousel'
+import CarouselSlide from './components/carousel/CarouselSlide'
+
+export default {
+  components: {
+    Todos,
+    Carousel,
+    CarouselSlide
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/carousel/Carousel.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="carousel">
+		<slot></slot>
+		<button class="carousel__nav carousel__next" @click.prevent="next">Suivante</button>
+		<button class="carousel__nav carousel__prev" @click.prevent="prev">Précédente</button>
+
+	</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+			index: 0,
+			slides: []
+		}
+	},
+	mounted () {
+		this.slides = this.$children
+		this.slides.forEach((slide, i) => {
+			slide.index = i
+		})
+	},
+	computed: {
+		slidesCount () {
+			return this.slides.length
+		}
+	},
+	methods: {
+		next () {
+			this.index++
+			if (this.index >= this.slidesCount){
+				this.index = 0
+			}
+		},
+		prev () {
+			this.index--
+			if (this.index < 0) {
+				this.index = this.slidesCount -1
+			}
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
